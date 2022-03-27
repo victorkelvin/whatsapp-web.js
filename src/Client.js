325b20b1e097190e6b7bce0f08174818238261ec
Victor Kelvin <victorkelvin@gmail.com>
2022-03-27 16:15:00 -0300

    switch back to puppeteer

--- src/Client.js
+++ src/Client.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const EventEmitter = require('events');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 const moduleRaid = require('@pedroslopez/moduleraid/moduleraid');
 
 const Util = require('./util/Util');
